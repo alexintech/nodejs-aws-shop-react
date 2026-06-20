a9c3ee537a35fd5b981c94a46f01d9a9f49e922f
Alexander Cherepanov <xandr.cherepanov@gmail.com>
2026-06-20 10:50:27 +0500

    task-9: use AWS EB for cart and order

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://8vu5lh7wgf.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://ml057dgc2a.execute-api.us-east-1.amazonaws.com/prod/api",
+  order: "http://alexintech-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
   import: "https://buic89amtj.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://ml057dgc2a.execute-api.us-east-1.amazonaws.com/prod/api",
+  cart: "http://alexintech-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
