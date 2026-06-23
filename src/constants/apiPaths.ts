d0c877d414552cd8f3be6025f51574e9fd0d9c70
Alexander Cherepanov <xandr.cherepanov@gmail.com>
2026-06-23 14:00:07 +0500

    use CloudFront for https requests for EB endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,13 @@
 const API_PATHS = {
   product: "https://8vu5lh7wgf.execute-api.us-east-1.amazonaws.com/prod",
-  order: "http://alexintech-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
+  // CloudFront for EB endpoint http://alexintech-cart-api-prod.us-east-1.elasticbeanstalk.com/api
+  // to fix https calls in frontend
+  order: "https://dfhrg3g2hmhuo.cloudfront.net/api",
   import: "https://buic89amtj.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://alexintech-cart-api-prod.us-east-1.elasticbeanstalk.com/api",
+  // CloudFront for EB endpoint http://alexintech-cart-api-prod.us-east-1.elasticbeanstalk.com/api
+  // to fix https calls in frontend
+  cart: "https://dfhrg3g2hmhuo.cloudfront.net/api",
 };
 
 export default API_PATHS;
